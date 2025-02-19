487cfd246d5a9633c8a75f0fe23573967fd860b9
Tang Bi Yang <by97@Tangs-MacBook-Air.local>
2025-02-19 14:35:09 +0800

    Initial commit - Sharedule project setup

---
+++ src/main/java/com/sharedule/app/ShareduleApplication.java
@@ -0,0 +1,13 @@
+package com.sharedule.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareduleApplication.class, args);
+	}
+
+}
