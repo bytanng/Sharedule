6e185a0606de7c92335d66e80ee1035fd0983410
Tang Bi Yang <by97@Tangs-MacBook-Air.local>
2025-02-26 16:37:44 +0800

    feat: Add user dropdown menu, logout functionality and password reset page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
+    
+    useEffect(() => {
+        // Add hover functionality to dropdowns
+        const dropdownElements = document.querySelectorAll('.dropdown');
+        
+        dropdownElements.forEach(dropdown => {
+            dropdown.addEventListener('mouseenter', () => {
+                dropdown.querySelector('.dropdown-menu').classList.add('show');
+            });
+            
+            dropdown.addEventListener('mouseleave', () => {
+                dropdown.querySelector('.dropdown-menu').classList.remove('show');
+            });
+        });
+        
+        // Cleanup event listeners on component unmount
+        return () => {
+            dropdownElements.forEach(dropdown => {
+                dropdown.removeEventListener('mouseenter', () => {});
+                dropdown.removeEventListener('mouseleave', () => {});
+            });
+        };
+    }, []);
+    
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> React Ecommerce</NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> Sharedule</NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -30,12 +54,28 @@ const Navbar = () => {
                     <div className="buttons text-center">
                         <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
                         <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cart ({state.length}) </NavLink>
+                        <div className="dropdown d-inline-block">
+                            <button className="btn btn-outline-dark m-2 dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i className="fa fa-user mr-1"></i> User123
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="userDropdown">
+                                <li><NavLink className="dropdown-item" to="/profile"><i className="fa fa-user-circle me-2"></i>My Profile</NavLink></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><NavLink className="dropdown-item" to="/logout"><i className="fa fa-sign-out-alt me-2"></i>Log Out</NavLink></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
 
             </div>
+            
+            <style jsx>{`
+                .dropdown:hover .dropdown-menu {
+                    display: block;
+                    margin-top: 0;
+                }
+            `}</style>
         </nav>
     )
 }
--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../components";
 
-const Login = () => {
+const ForgetPassword = () => {
   return (
     <>
       <Navbar />
       <div className="container my-3 py-3">
-        <h1 className="text-center">Login</h1>
+        <h1 className="text-center">Reset Password</h1>
         <hr />
         <div class="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
@@ -21,22 +21,9 @@ const Login = () => {
                   placeholder="name@example.com"
                 />
               </div>
-              <div class="my-3">
-                <label for="floatingPassword display-4">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="floatingPassword"
-                  placeholder="Password"
-                />
-              </div>
-              <div className="my-3">
-                <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
-                <p>Forget Your Password? <Link to="/forgetpassword" className="text-decoration-underline text-info">Reset Here</Link> </p>
-              </div>
               <div className="text-center">
                 <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
-                  Login
+                    Reset
                 </button>
               </div>
             </form>
@@ -48,4 +35,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ForgetPassword;
