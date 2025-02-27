ca4523ee2983dfeb8c9b055c3bb045a3710efce5
Benjamin Tay <benjamin.tay@ncs.com.sg>
2025-02-27 21:16:06 +0800

    feat: disable authorization for reset password routes

--- src/main/java/com/sharedule/app/config/SecurityConfig.java
+++ src/main/java/com/sharedule/app/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         return http
             .csrf(customizer -> customizer.disable())
             .authorizeHttpRequests(request -> request
-            .requestMatchers("register", "login", "logout")
+            .requestMatchers("register", "login", "logout", "user/reset-password")
             .permitAll()
             .anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults())
