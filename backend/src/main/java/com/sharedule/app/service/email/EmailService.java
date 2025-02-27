53abe8384592b6690ba9a27a6313093feabdf721
Benjamin Tay <benjamin.tay@ncs.com.sg>
2025-02-27 21:57:48 +0800

    chore: clean up code

--- backend/src/main/java/com/sharedule/app/service/email/EmailService.java
+++ backend/src/main/java/com/sharedule/app/service/email/EmailService.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.sharedule.app.service.email;
 
 import org.springframework.beans.factory.annotation.Value;
