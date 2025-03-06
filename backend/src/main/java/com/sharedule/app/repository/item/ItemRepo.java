47f930070b8977a66d6e2af2441a138a7241a119
Jacobnbh <jacobngbh@hotmail.com>
2025-03-06 22:53:46 +0800

    add findById to query items from the repo based on their ID

--- backend/src/main/java/com/sharedule/app/repository/item/ItemRepo.java
+++ backend/src/main/java/com/sharedule/app/repository/item/ItemRepo.java
@@ -5,9 +5,11 @@ import com.sharedule.app.model.item.Item;
 import com.sharedule.app.model.user.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ItemRepo extends MongoRepository<Item, String> {
     List<Item> findByUser(Users user);
     List<Item> findByUser_Id(String userId);
+    Optional<Item> findById(String itemId);
 }
