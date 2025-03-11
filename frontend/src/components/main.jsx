5d594ce47b4d705e615c5fba1e92e87608b3e5a5
Jacobnbh <jacobngbh@hotmail.com>
2025-03-11 15:40:50 +0800

    update Home page description

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -13,11 +13,20 @@ const Home = () => {
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <h5 className="card-title fs-1 text fw-lighter">Sharedule</h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Sharedule is a dynamic marketplace platform designed to
+                streamline transactions by integrating seamless scheduling
+                capabilities. Unlike many existing platforms, it enables sellers
+                to showcase their services or products while efficiently
+                managing availability and accepting appointments in real-time.
+                Buyers can explore offerings, book appointments effortlessly,
+                and complete transactions with ease. Key features such as
+                automated reminders, real-time notifications, and a feedback
+                system enhance user experience and foster trust. By prioritizing
+                convenience, clarity, and efficiency, Sharedule aims to
+                revolutionize how buyers and sellers connect in today’s
+                service-driven economy.
               </p>
             </div>
           </div>
