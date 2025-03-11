614bb13c3bda0c12aebcc7fa47fac9f23bd1c861
Jacobnbh <jacobngbh@hotmail.com>
2025-03-11 13:49:32 +0800

    update Home.jsx to remove Product page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Navbar, Main, Product, Footer } from "../components";
+import { Navbar, Main, Footer } from "../components";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Main />
-      <Product />
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
