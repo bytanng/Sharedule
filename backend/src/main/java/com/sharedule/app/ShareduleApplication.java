2d44ff7dd0b351f151691d4ccaad1751a3f444b3
Eykf1997 <76686621+Eykf1997@users.noreply.github.com>
2025-04-25 23:12:04 +0800

    Update ShareduleApplication.java

--- backend/src/main/java/com/sharedule/app/ShareduleApplication.java
+++ backend/src/main/java/com/sharedule/app/ShareduleApplication.java
@@ -4,7 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+  MongoAutoConfiguration.class, 
+  MongoDataAutoConfiguration.class
+})
 public class ShareduleApplication {
 
 	public static void main(String[] args) {
