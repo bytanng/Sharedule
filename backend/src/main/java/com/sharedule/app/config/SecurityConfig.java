930bf48e2b6e3a78c9148021af1d92ca00cc7bb3
Eykf1997 <76686621+Eykf1997@users.noreply.github.com>
2025-04-26 11:42:01 +0800

    Update SecurityConfig.java

--- backend/src/main/java/com/sharedule/app/config/SecurityConfig.java
+++ backend/src/main/java/com/sharedule/app/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/register", "/login", "/logout", "/user/profile", "/s3", "/user/reset-password",
-                                "/file", "/products", "/products/search", "/product/*")
+                                "/file", "/products", "/products/search", "/product/*","/actuator/health")
                         .permitAll().requestMatchers("/file/**").authenticated()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
