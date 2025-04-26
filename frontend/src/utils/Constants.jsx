c04997bb16003552960fa785aa0efa1e19f28dd9
Eykf1997 <76686621+Eykf1997@users.noreply.github.com>
2025-04-26 14:50:22 +0800

    Update Constants.jsx

--- frontend/src/utils/Constants.jsx
+++ frontend/src/utils/Constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = "http://my-backend-alb-841158039.us-east-1.elb.amazonaws.com";
 
 export const POST_METHOD = 'POST';
 export const GET_METHOD = 'GET';
